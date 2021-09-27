72a247d9ba0a8a4346c04f9df1df1bf684975b12
Leo Ris <ristanovski@hotmail.com>
2021-09-28 00:05:25 +0200

    Demo for CodeFirst approach.

---
+++ CodeFirst/Migrations/202109270719376_CreateAddress.cs
@@ -0,0 +1,34 @@
+﻿namespace CodeFirst.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateAddress : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Addresses",
+                c => new
+                    {
+                        AddressId = c.Int(nullable: false, identity: true),
+                        Street = c.String(),
+                        Number = c.String(),
+                        PostalCode = c.String(),
+                        City = c.String(),
+                        State = c.String(),
+                        Province = c.String(),
+                        Country = c.String(),
+                        IsActive = c.Int(),
+                        IsDeleted = c.Int(),
+                    })
+                .PrimaryKey(t => t.AddressId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Addresses");
+        }
+    }
+}
---
+++ CodeFirst/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirst
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string PostalCode { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+        public Nullable<int> IsActive { get; set; }
+        public Nullable<int> IsDeleted { get; set; }
+    }
+
+    public class AddressDbContext : DbContext
+    {
+        public DbSet<Address> Addresses { get; set; }                         
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
